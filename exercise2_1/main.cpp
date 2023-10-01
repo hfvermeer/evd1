850455a4cb771e46d16109876b794711e18a4aa5
Beagle User <debian@BeaglePlay.localdomain>
2023-10-01 11:20:29 +0000

    Convert to relative file path

--- exercise2_1/main.cpp
+++ exercise2_1/main.cpp
@@ -6,7 +6,7 @@ int main(int, char**) {
     std::cout << "Exercise 2_1, OpenCV version: " << CV_VERSION << std::endl;
 
     // Use dark_flower.bmp in local folder
-    cv::Mat inputImage = cv::imread("C:/embedded/evd1/exercise2_1/dark_flower.bmp", cv::IMREAD_COLOR);
+    cv::Mat inputImage = cv::imread("./dark_flower.bmp", cv::IMREAD_COLOR);
     
     std::vector<cv::Mat> channels;
     cv::split(inputImage, channels);
@@ -17,9 +17,9 @@ int main(int, char**) {
     cv::imshow("Red", channels[2]);
 
     // Save each channel as a separate image
-    cv::imwrite("C:/embedded/evd1/exercise2_1/blue.bmp", channels[0]);
-    cv::imwrite("C:/embedded/evd1/exercise2_1/green.bmp", channels[1]);
-    cv::imwrite("C:/embedded/evd1/exercise2_1/red.bmp", channels[2]);
+    cv::imwrite("./blue.bmp", channels[0]);
+    cv::imwrite("./green.bmp", channels[1]);
+    cv::imwrite("./red.bmp", channels[2]);
 
     // Wait for a key press, then kill all windows
     cv::waitKey(0);
@@ -38,13 +38,15 @@ int main(int, char**) {
     cv::imshow("Value", channels[2]);
 
     // Save each channel as a separate image
-    cv::imwrite("C:/embedded/evd1/exercise2_1/hue.bmp", channels[0]);
-    cv::imwrite("C:/embedded/evd1/exercise2_1/saturation.bmp", channels[1]);
-    cv::imwrite("C:/embedded/evd1/exercise2_1/value.bmp", channels[2]);
+    cv::imwrite("./hue.bmp", channels[0]);
+    cv::imwrite("./saturation.bmp", channels[1]);
+    cv::imwrite("./value.bmp", channels[2]);
 
     // Wait for a key press, then kill all windows
     cv::waitKey(0);
     cv::destroyAllWindows();
+
+    std::cout << "Done!" << std::endl;
 
     return 0;
 }
