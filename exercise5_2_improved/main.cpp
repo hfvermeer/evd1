66921c964e6697838404e28e6f599334484fcc44
Herre Vermeer <herre.vermeer@gmail.comgit config --global user.name Herre>
2023-10-15 21:34:44 +0200

    improve upon exercise

---
+++ exercise5_2_improved/main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+/**
+ * Helper function to display text in the center of a contour
+ */
+void setLabel(Mat& im, const string label, vector<Point>& contour)
+{
+    int fontface = FONT_HERSHEY_SIMPLEX;
+    double scale = 0.4;
+    int thickness = 1;
+    int baseline = 0;
+
+    Size text = getTextSize(label, fontface, scale, thickness, &baseline);
+    Rect r = boundingRect(contour);
+
+    Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+    rectangle(im, pt + Point(0, baseline), pt + Point(text.width, -text.height), Scalar(255,255,255), FILLED);
+    putText(im, label, pt, fontface, scale, Scalar(0,0,0), thickness, 8);
+}
+
+
+int main(int, char**) {
+    std::cout << "Exercise 5_2, OpenCV version: " << CV_VERSION << std::endl;
+
+    Mat src;
+
+    // Mat src=imread("../shapes.jpg");
+    // if(!src.data) {
+    //     std::cout << "Could not open image!" << CV_VERSION << std::endl;
+    // }
+    // else {
+        Mat gray,bw,dst,guassian,tres;
+        vector<vector<Point> > contours;
+        vector<Point> approx;
+
+        VideoCapture capture(0);
+
+        namedWindow("Blurry", WINDOW_AUTOSIZE);
+        moveWindow("Blurry", 50, 150);
+
+        namedWindow("Threshold", WINDOW_AUTOSIZE);
+        moveWindow("Threshold", 50, 200);
+
+        namedWindow("Edges", WINDOW_AUTOSIZE);
+        moveWindow("Edges", 50, 50);
+
+        namedWindow("dst", WINDOW_AUTOSIZE);
+        moveWindow("dst", 50, 100);
+
+
+        while(waitKey(30) != 27)
+        {
+            capture >> src;
+            // Convert to grayscale
+            cvtColor(src, gray, COLOR_BGR2GRAY);
+
+            // Use gaussian blur to remove noise
+            GaussianBlur(gray, guassian, Size(5, 5), 0);
+            imshow("Blurry", guassian);
+
+            // Use threshold to binarize the image
+            threshold(guassian, tres, 100, 255, THRESH_BINARY);
+            imshow("Threshold", tres);
+
+            // Use Canny instead of threshold to catch squares with gradient shading
+            Canny(tres, bw, 80, 240, 3);
+            imshow("Edges", bw);
+
+            // Find contours
+            findContours(bw, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);        
+
+            src.copyTo(dst);
+            for (unsigned int i = 0; i < contours.size(); i++)  // Repeat the number of contours (.size)
+            {
+                // Approximate contour with accuracy proportional
+                // to the contour perimeter
+                approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
+
+                // Skip small or non-convex objects (closed polygon)
+                if ((contourArea(contours[i])) < 100 || !isContourConvex(approx))
+                    continue;
+
+                // Draw the in green contour on dst
+                drawContours(dst, contours, i, Scalar(0, 255, 0), 2);
+
+                // Draw the approximated polygon in blue
+                drawContours(dst, vector<vector<Point> >{approx}, -1, Scalar(255, 0, 0), 2);
+
+                // Draw corner points in red
+                for (unsigned int j = 0; j < approx.size(); j++)
+                    circle(dst, approx[j], 3, Scalar(0, 0, 255), -1);
+
+                // Label detected objects
+                if (approx.size() >= 3 && approx.size() <= 6) {
+                    switch (approx.size()) {
+                    case 3 : setLabel(dst, "TRIANGEL", contours[i]);
+                        break;
+                    case 4:  setLabel(dst, "RECTANGLE", contours[i]);
+                        break;
+                    case 5:  setLabel(dst, "PENTAGON", contours[i]);
+                        break;
+                    case 6:  setLabel(dst, "HEXAGON", contours[i]);
+                        break;
+                    }
+                }
+                else {
+                    // Detect and label circles
+                    double area = contourArea(contours[i]);
+                    Rect r = boundingRect(contours[i]);
+                    int radius = r.width / 2;
+
+                    if (abs(1 - (static_cast<double>(r.width) / static_cast<double>(r.height))) <= 0.2 &&
+                            abs(1 - (area / (CV_PI * (radius*radius)))) <= 0.2)
+                        setLabel(dst, "CIRCLE", contours[i]);
+                }
+            }
+            imshow("dst", dst);
+        }
+        destroyAllWindows();
+
+        std::cout << "Done!" << std::endl;
+
+    // }
+
+    return 0;
+}
